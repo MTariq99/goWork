51d3c158c7878c8d88a8390fcf7a47fa9f218e87
muhammad Tariq <tariqwc99@gmail.com>
2023-02-23 11:55:47 +0500

    get post and delete apis

--- main.go
+++ main.go
@@ -12,7 +12,36 @@ func main() {
 	router.GET("/books", listBookHandler)
 	router.POST("/books", createBookHandler)
 	router.DELETE("/books/:id", deleteBookHandler)
+	// router := gin.New()
+	// router.GET("/", func(c *gin.Context) {
+	// 	c.JSON(http.StatusOK, gin.H{
+	// 		"message": "hello world!",
+	// 	})
+	// })
+	// router.GET("/books", func(c *gin.Context) {
+	// 	c.JSON(http.StatusOK, books)
+	// })
+	// router.POST("/books", func(c *gin.Context) {
+	// 	var book Book
+	// 	if err := c.ShouldBindJSON(&book); err != nil {
+	// 		c.JSON(http.StatusBadRequest, gin.H{
+	// 			"ERROR": err.Error(),
+	// 		})
+	// 	}
+	// 	books = append(books, book)
+	// 	c.JSON(http.StatusOK, book)
+	// })
+	// router.DELETE("books/:id", func(c *gin.Context) {
+	// 	id := c.Param("id")
+	// 	for i, a := range books {
+	// 		if a.ID == id {
+	// 			books = append(books[:i], books[i+1:]...)
+	// 			break
+	// 		}
+	// 	}
+	// 	c.Status(http.StatusNoContent)
 
+	// })
 	fmt.Println("the server is Running on Port:http://localhost:8080/ ")
 	router.Run()
 }
